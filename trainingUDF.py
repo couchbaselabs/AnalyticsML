9657ee782f0245d2316ca2d12112eaaaecbf3739
mks-couchbase <muk.sreenivasan@couchbase.com>
2021-09-28 14:24:03 -0500

    updated for go live

---
+++ trainingUDF.py
@@ -0,0 +1,49 @@
+### This is the what is used to training the model based the file in the .csv
+
+from pandas import read_csv
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.linear_model import LogisticRegression
+import pickle
+
+def getData():
+        review_train = read_csv("rotten_tomatoes_critic_reviews_train.csv", sep=",", encoding="ISO-8859-1")
+        review_train = review_train.rename(index=str,
+                                             columns={"rotten_tomatoes_link": "id", "review_type": "sentiment", "review_content": 
+                                                      "text"})
+        return review_train
+
+if __name__ == '__main__':
+        critics = getData()
+        X = critics["text"]
+        y = critics["sentiment"]
+
+        tf_transformer = TfidfTransformer()
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=111, test_size=0.2)
+
+        model = Pipeline([
+            ('vectorizer', CountVectorizer()),
+            ('transformer', TfidfTransformer()),
+            ('classifier', LogisticRegression(solver='sag',multi_class='multinomial'))
+        ])
+
+        model.fit(X_train, y_train)
+        predictions = model.predict(X_test)
+
+        print(confusion_matrix(y_test,predictions))
+        print(classification_report(y_test,predictions))
+        print("Overall accuracy: {}".format(accuracy_score(y_test, predictions)))
+
+        ret = model.predict(["So let's recap: We've broken into Buckingham Palace, and the Oval Office, stolen a page from the President's super-secret book, and actually kidnapped the President of the United States. What are we gonna do next, short-sheet the Pope's bed?"])[-1]
+        print(ret)
+        ret = model.predict(["The movie is bad"])[0]
+        print(ret)
+        ret = model.predict(["The movie is alright"])[0]
+        print(ret)
+
+        pickle.dump(model, open("pipelines/sentiment_model", 'wb'), protocol=1)
+
